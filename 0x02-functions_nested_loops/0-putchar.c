3f593a112ecdcce4ee21cdbfa36ce732db7db5c8
Mfoniso Christopher <cmfoniso@gmail.com>
2023-08-16 19:03:55 -0700

    done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,21 @@
 #include "main.h"
-	/**
-	 * main - entry point
-	 * 
-	 * description:a function that prints the alphabet
-	 * Return: 0(success)
-	 */
+/**
+ * main - print _putchar
+ * is written
+ * Return: 0
+ */
 
-
-	int main(void)
-	{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
-	}
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
 
